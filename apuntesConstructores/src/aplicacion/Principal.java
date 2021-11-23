fb8cd723cd00269273f9b3f35c19315adecfeeee
Carlos Moragon <cmoragoncorella@gmail.com>
2021-11-23 19:09:02 +0100

    añadido 1 comentario

--- apuntesConstructores/src/aplicacion/Principal.java
+++ apuntesConstructores/src/aplicacion/Principal.java
@@ -8,6 +8,7 @@ import dominio. *;
 public class Principal{
 	public static void main(String[] args){
 		//CONSTRUCTORES SE UTILIZAN CON CLASES
+		//Esto es un ejemplo de como utilizar constructores en la clase principal
 		
 		Clase mates = new Clase("matematicas",54);
 		Clase programacion = new Clase("Programacion", 123);
